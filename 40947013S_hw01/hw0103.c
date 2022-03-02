66c393cb2882b84fbfc94384c6c40e5608e8b05a
sun-wt <71592704+sun-wt@users.noreply.github.com>
2022-03-02 16:34:43 +0800

    Update hw0103.c

--- 40947013S_hw01/hw0103.c
+++ 40947013S_hw01/hw0103.c
@@ -2,13 +2,13 @@
 
 int main()
 {   
-    char szNumbers[] = "2001 60c0c0 -1101110100110100100000 0x6fffff";
+    /*char szNumbers[] = "2001 60c0c0 -1101110100110100100000 0x6fffff";
     char * pEnd;
     long int li1, li2, li3, li4;
     li1 = mystrtol (szNumbers,&pEnd,10);
     li2 = mystrtol (pEnd,&pEnd,16);
     li3 = mystrtol (pEnd,&pEnd,2);
     li4 = mystrtol (pEnd,NULL,0);
-    printf ("转换成10进制: %ld、%ld、%ld、%ld\n", li1, li2, li3, li4);
+    printf ("转换成10进制: %ld、%ld、%ld、%ld\n", li1, li2, li3, li4);*/
     return 0;
 }
